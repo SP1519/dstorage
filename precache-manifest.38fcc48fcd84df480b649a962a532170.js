7fbd9b836ac4fd9c89f159ed5459073dd1b97b41
Sergej <s.penner1501@hotmail.com>
2023-01-22 04:19:20 +0100

    Updates

---
+++ precache-manifest.38fcc48fcd84df480b649a962a532170.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4acb2495f3675caa7dde622b78b73c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac319b5ba7be70b33ac3",
+    "url": "./static/css/2.e1f817c4.chunk.css"
+  },
+  {
+    "revision": "7d32e83dd2e3cc09e930",
+    "url": "./static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "ac319b5ba7be70b33ac3",
+    "url": "./static/js/2.ea56e98a.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.ea56e98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d32e83dd2e3cc09e930",
+    "url": "./static/js/main.3aea0bf5.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
