587e5a7fc9e20aefc714f58d7376a07c5a43eb9e
Sergej <s.penner1501@hotmail.com>
2023-01-22 04:20:18 +0100

    Adds github pages deployment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-//import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
 import Navbar from './Navbar'
 import Main from './Main'
@@ -6,6 +6,8 @@ import Web3 from 'web3';
 import './App.css';
 
 //Declare IPFS
+const ipfsClient = require ("ipfs-http-client")
+const ipfs = ipfsClient({host:"ipfs.infura.io", port:5001, protocol: "https"})
 
 class App extends Component {
 
@@ -15,15 +17,50 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    //Setting up Web3
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('Non-Ethereum browser. You should consider trying MetaMask')
+    }
   }
 
+
+
   async loadBlockchainData() {
-    //Declare Web3
+    const web3 = window.web3
+    console.log(web3)
 
     //Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+    //console.log(accounts)
 
-    //Network ID
+//create javascript version 
+    //Get Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DStorage.networks[networkId]
+
+    if (networkData){
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+      this.setState({filesCount})
+
+      const filesCount = await dstorage.methods.fileCount().call()
+      this.setState({filesCount})
+
+      for(var i = filesCount; i>=1;i--) {
+        const file = await dstorage.methods.files(i).call()
+        this.setState({
+          files:[...this.state.files, file]
+        })
+      }
+    } else {
+      window.alert('DStorage contract not deployed to detected network.')
+    }
 
     //IF got connection, get data from contracts
       //Assign contract
@@ -39,13 +76,53 @@ class App extends Component {
 
   // Get file from user
   captureFile = event => {
+    event.preventDefault()
+
+    const file= event.target.files[0]
+    const reader = new window.FileReader()
+
+    reader.readAsArrayBuffer (file)
+    reader.onloadend = () =>{
+      this.setState({
+        buffer: Buffer(reader.result),
+        type: file.type,
+        name: file.name
+      })
+      console.log("buffer", this.state.buffer)
+    }
   }
 
 
   //Upload File
   uploadFile = description => {
-
+    console.log("Sending file to IPFS")
     //Add file to the IPFS
+    ipfs.add(this.state.buffer, (error,result)=>{
+      console.log("IPFS result", result) //result?
+
+      if (error){
+        console.error(error)
+        return
+      }
+
+      this.setState({loading:true})
+      
+      if (this.state.type === "")
+        this.setState({type: "none"})
+
+      this.state.dstorage.methods.uploadFile(result[0].hash, result[0].size, this.state.type, this.state.name, description).send({from: this.state.account}).on("transactionHash", (hash)=>{
+        this.setState({
+          loading:false,
+          type:null,
+          name:null
+        })
+      window.location.reload()
+    }).on("error", (e) =>{
+      window.alert("Error")
+      this.setState({loading:false})
+      })
+    })//1:06:00
+
 
       //Check If error
         //Return error
@@ -62,6 +139,12 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      account:"",
+      dstorage:null,
+      files:[],
+      loading: false,
+      type: null,
+      name: null
     }
 
     //Bind functions
--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,76 @@
 pragma solidity ^0.5.0;
 
-contract DStorage {
-  // Name
-  // Number of files
+contract dstorage {
+  string public name= "DStorage";
+  uint public fileCount=0;
+    // Number of files
   // Mapping fileId=>Struct 
+  mapping(uint => File) public files;
+
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime; //now
+    address payable uploader; //msg.sender
+  }
+
+
 
   // Struct
 
 
-  // Event
+  // Event - stores arguments passed in log. inform about current state of contract
+  event FileUploaded (
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+    );
 
   constructor() public {
   }
 
-  // Upload File function
+  function uploadFile (string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+    require (bytes(_fileHash).length>0);// Make sure the file hash exists
 
-    // Make sure the file hash exists
+    require (bytes(_fileType).length>0);// Make sure file type exists
 
-    // Make sure file type exists
+    require (bytes(_fileDescription).length>0);// Make sure file description exists
 
-    // Make sure file description exists
+    require (bytes(_fileName).length>0);// Make sure file fileName exists
 
-    // Make sure file fileName exists
+    require (msg.sender!=address(0));// Make sure uploader address exists
 
-    // Make sure uploader address exists
+    require (_fileSize>0);// Make sure file size is more than 0
+      
+    fileCount++;
 
-    // Make sure file size is more than 0
+    files[fileCount]= File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender); //"now" = timetamp
 
 
     // Increment file id
 
     // Add File to the contract
 
-    // Trigger an event
+    
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);// Trigger an event
+}
+
+
 
 }
+
+
+//truffle migrate --reset - puts new copy of smart contract on blockchain instead of changing existing (--reset)
+//npm run start -web dev environment
+
+//meta mask connects browser to blockchain
+//web3js library connects web application to blockchain
